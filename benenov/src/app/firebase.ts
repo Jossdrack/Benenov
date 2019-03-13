c5c3ea289c634c4377ec7a580a239bf4ae43ef32
benenov <hardymilalu@MacBook-Pro-de-Hardy-2.local>
2019-03-13 10:34:41 +0100

    structure projet

---
+++ benenov/src/app/firebase.ts
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyDqheXwh_iD7Md87cJE7c87aHDD3KDXMmU",
+    authDomain: "benenov-2e32b.firebaseapp.com",
+    databaseURL: "https://benenov-2e32b.firebaseio.com",
+    projectId: "benenov-2e32b",
+    storageBucket: "benenov-2e32b.appspot.com",
+    messagingSenderId: "190553061085"
+  };
+
+  export default config
